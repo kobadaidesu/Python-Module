ab211f6fe411a6b8381166c186a65e6e2b1a303c
kobadaidesu <daigokobayashi04@gmail.com>
2026-06-18 19:08:09 +0900

    Complete growing code exercises

---
+++ module00/ex6/ft_count_harvest_recursive.py
@@ -0,0 +1,11 @@
+def ft_count_harvest_recursive():
+    days = int(input("Days until harvest: "))
+
+    def count_day(i):
+        if i > days:
+            return
+        print("Day", i)
+        count_day(i + 1)
+
+    count_day(1)
+    print("Harvest time!")
